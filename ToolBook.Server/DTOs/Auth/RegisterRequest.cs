947532d697ee0442e8c06e6bd26b7c3af91378e8
Mathias <mathias@schaltz.dev>
2026-09-12 04:40:28 +0200

    added requirements for login

--- ToolBook.Server/DTOs/Auth/RegisterRequest.cs
+++ ToolBook.Server/DTOs/Auth/RegisterRequest.cs
@@ -1,8 +1,17 @@
-﻿namespace ToolBook.Server.DTOs.Auth;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToolBook.Server.DTOs.Auth;
 
 public class RegisterRequest
 {
+    [Required]
     public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
     public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [MinLength(8)]
     public string Password { get; set; } = string.Empty;
 }
